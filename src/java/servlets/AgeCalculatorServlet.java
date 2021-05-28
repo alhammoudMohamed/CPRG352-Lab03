12c2380e8c3e19bb48856f3e560411761992bc7b
844568 <844568@SAIT227582IT.ACDM.DS.SAIT.CA>
2021-05-28 12:01:53 -0600

    First commit Age calculator done

---
+++ src/java/servlets/AgeCalculatorServlet.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 844568
+ */
+public class AgeCalculatorServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String userAge = request.getParameter("age");
+
+        request.setAttribute("Age", userAge);
+
+        if (!isNumeric(userAge)) {
+            request.setAttribute("message", "Invalid entry.Please enter a number");
+            getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp.jsp").forward(request, response);
+            return;
+        }
+
+        int ageValue = Integer.parseInt(userAge);
+        request.setAttribute("ageNextBirthday", ++ageValue);
+
+        getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp").forward(request, response);
+
+    }
+
+    public static boolean isNumeric(String value) {
+        double numberValue;
+
+        if (value == null || value.equals("")) {
+            System.out.println("The value that the user entered is null or empty");
+            return false;
+        }
+        try {
+            numberValue = Double.parseDouble(value);
+            return true;
+        } catch (NumberFormatException ex) {
+            System.out.println("The entery cannot be parsed to an integer");
+        }
+        return false;
+    }
+
+}
---
+++ web/WEB-INF/ageCalculator.jsp
@@ -0,0 +1,34 @@
+<%-- 
+    Document   : ageCalculator
+    Created on : May 26, 2021, 1:49:11 PM
+    Author     : 844568
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Age Calculator</title>
+    </head>
+    <body>
+        <header> 
+            <p>This magical calculator will tell you your age on your next birthday!</p>
+        </header>
+        <h1>Age Calculator</h1>
+
+
+        <form method = "post" > 
+            <label>Enter your age:  </label>
+            <input type="number" name="age"  step="1" min="1" max="130" placeholder="1"> </input>
+            <br>
+            <input type="submit" value= "Age next birthdays"> </input>
+
+            <p> Your age on your next birthday is ${ageNextBirthday}</p>
+
+            <p>${error_message}</p>
+
+            <a href=""></a>
+        </form>
+    </body>
+</html>
